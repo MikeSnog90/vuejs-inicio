3b7075f2fa681dc4bbc2b92b729ba449b39b48ee
Michael Nogueira <michaelnogueira.8237@aluno.saojudas.br>
2021-11-17 23:16:43 -0300

    aula 11

--- 11-Sintaxe de template-interpolação/script.js
+++ 11-Sintaxe de template-interpolação/script.js
@@ -1,1 +1,12 @@
-// inicio
+let app = new Vue({
+  el: "#app",
+  data: {
+    titulo: "titulo da página",
+    subtitulo: "Este é o <strong>subtítulo</strong> da página",
+    copyright: "todos os direitos reservados",
+    numero: 10,
+    nome: "Michael",
+
+    carregado: true,
+  },
+});
